61791efd0ad81f21607b87b583d891b7d6968670
ReyGee <ronit@apploperstechnologies.com>
2020-05-30 14:03:09 +1000

    hi

--- app_profile/views.py
+++ app_profile/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+import base64
 
 
 def home(request):
     myinfo = Myinfo.objects.get(id=1)
+    img = base64.b64encode(myinfo.picture)
     language = Language.objects.all()
     interest = Interest.objects.all()
     education = Education.objects.all()
     experience = Experience.objects.all()
+    skill = Skills.objects.all()
 
     # return HttpResponse('Hi i am learning aws')
     context = {'myinfo': myinfo, 'language': language, 'interest': interest, 'education': education,
-               'experience': experience}
+               'experience': experience, 'skill': skill}
 
     return render(request, 'dashboard.html', context)
 # Create your views here.
